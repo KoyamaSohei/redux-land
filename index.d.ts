0e7f0d936aad68d88b2d4acb6a988cc482a10542
KoyamaSohei <koyamaso0309@gmail.com>
2019-01-12 04:57:53 +0900

    add option to inject dependencies

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,19 @@
 import { Middleware, AnyAction, Dispatch, Store } from "redux";
 
-export type Land<State, Action extends AnyAction,O = AnyAction> = (
-  args: { state: State; action: Action }
+export type Land<State, Action extends AnyAction,O = AnyAction,Dependencies = any> = (
+  args: { state: State; action: Action },
+  dependencies: Dependencies
 ) => AsyncIterableIterator<O>;
 
 export type Lands = {
-  [key: string]: Land<any, any>;
+  [key: string]: Land<any, any, any>;
 };
 
 export declare function createLandMiddleware(
-  lands: Lands
+  lands: Lands,
+  dependencies?: {
+    [key: string]: any;
+  }
 ): Middleware<{}, any, Dispatch>;
 
 export default createLandMiddleware;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-function createLandMiddleware(land) {
+function createLandMiddleware(land, dependencies = {}) {
   return ({ getState }) => next => async action => {
     const state = getState();
     for (const type in land) {
       if (action.type == type) {
-        for await (const ac of land[type]({ state, action })) {
+        for await (const ac of land[type]({ state, action }, dependencies)) {
           next(ac);
         }
       }
